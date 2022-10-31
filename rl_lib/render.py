54acbc4e1d2dcd2620656819dca91f4d03cda25e
Trenten <tnorwat@gmail.com>
2022-10-31 15:30:57 -0500

    figured out lstm

--- rl_lib/render.py
+++ rl_lib/render.py
@@ -9,7 +9,7 @@ from ray import tune
 # checkpoint_path = 'models/1667202595/checkpoint_000001/checkpoint-1' 
 # checkpoint_path = 'snake_V1/PPO/PPO_Snake_04b80_00000_0_2022-10-31_01-34-06/checkpoint_000001/checkpoint-1' # using lstm
 # checkpoint_path = 'snake_V1/PPO/PPO_Snake_7a6de_00000_0_2022-10-31_01-44-33/checkpoint_000001/checkpoint-1' # not using lstm
-checkpoint_path = 'snake_V1/PPO/PPO_Snake_1f2ab_00000_0_2022-10-31_03-07-54/checkpoint_005129/checkpoint-5129' # not using lstm
+# checkpoint_path = 'snake_V1/PPO/PPO_Snake_84181_00000_0_2022-10-31_11-46-07/checkpoint_000001/checkpoint-1' # not using lstm
 # config = ppo.PPOConfig().framework('torch').training(model=dict(use_lstm=True,),).resources(num_gpus=1)
 
 
@@ -21,17 +21,55 @@ checkpoint_path = 'snake_V1/PPO/PPO_Snake_1f2ab_00000_0_2022-10-31_03-07-54/chec
 
 # algo = tune.Tuner().restore(checkpoint_path=checkpoint_path)
 
-agent = ppo.PPO(env=Snake, config={'framework': 'torch'})
-agent.restore(checkpoint_path=checkpoint_path)
+# agent = ppo.PPO(env=Snake, config={'framework': 'torch'})
+# agent = ppo.PPO(env=Snake, config={'framework': 'torch'})
+# agent.restore(checkpoint_path=checkpoint_path)
 # algo = Algorithm.load_checkpoint(checkpoint_path=checkpoint_path)
 # algo = ppo.PPO.load_checkpoint(checkpoint_path=checkpoint_path)
 
+
+config2 = {
+    'env': Snake,
+    'env_config': {},
+    'framework': 'torch',
+    'gamma': .99,
+    'lambda': .95,
+    'lr': 0.0003,
+    'vf_loss_coeff': .5,
+    'clip_param': .2,
+    'sgd_minibatch_size': 4096,
+    'train_batch_size': 32768,
+    'num_workers': 8,
+    'num_gpus': 1,
+    'grad_clip': .5,
+    'model': {
+        'use_lstm': True
+    },
+    'batch_mode': 'complete_episodes',
+    'evaluation_num_workers': 1,
+    'evaluation_config': {
+        'render_env': True
+    }
+}
 env = Snake(size=6, time_between_moves=100)
 obs = env.reset()
-while True:
-    action = agent.compute_single_action(obs)
-    obs, rewards, dones, info = env.step(action=action)
-    env.render(renderer=100)
-    if dones:
-        obs = env.reset()
-        env.render()
+
+
+checkpoint_path = 'models/checkpoint_000001/checkpoint-1'
+
+agent = ppo.PPO(config=config2)
+agent.load_checkpoint(checkpoint_path)
+
+print(agent.get_policy().export_model('exported'))
+agent.restore()
+# print(agent.get_config())
+
+# agent.evaluate()
+
+# while True:
+#     action = agent.compute_single_action(obs)
+#     obs, rewards, dones, info = env.step(action=action)
+#     env.render(renderer=100)
+#     if dones:
+#         obs = env.reset()
+#         env.render()
--- rl_lib/train.py
+++ rl_lib/train.py
@@ -1,10 +1,11 @@
+from tabnanny import check
 import ray
 from ray.rllib.agents.ppo import PPOTrainer
 from ray.rllib.algorithms.ppo import PPO
 import time
 from Snake import Snake, Actions
 from ray.tune.logger import TBXLoggerCallback
-from ray import tune
+from ray import tune, air
 import ray.rllib.algorithms.ppo as ppo
 
 models_dir = f'models/{int(time.time())}/'
@@ -45,9 +46,32 @@ config2 = {
     'model': {
         'use_lstm': True
     },
-    'batch_mode': 'complete_episodes'
+    'batch_mode': 'complete_episodes',
+    'evaluation_num_workers': 1,
+    'evaluation_config': {
+        'render_env': True
+    }
 }
 
+config3 = {
+    'env': Snake,
+    'env_config': {},
+    'framework': 'torch',
+    'gamma': .99,
+    'lambda': .95,
+    'lr': 0.0003,
+    'vf_loss_coeff': .5,
+    'clip_param': .2,
+    'sgd_minibatch_size': 4096,
+    'train_batch_size': 32768,
+    'num_workers': 8,
+    'num_gpus': 1,
+    'grad_clip': .5,
+    'model': {
+        'use_lstm': True
+    },
+    'batch_mode': 'complete_episodes'
+}
 
 # ray.shutdown()
 # need 1 more cpus to workers 
@@ -76,26 +100,29 @@ ray.init(
 # checkpoint_freq=1)
 
 
-tune.run('PPO', config={
-    'env': Snake,
-    'framework': 'torch',
-    'gamma': .99,
-    'lambda': .95,
-    'lr': 0.0003,
-    'vf_loss_coeff': .5,
-    'clip_param': .2,
-    'sgd_minibatch_size': 4096,
-    'train_batch_size': 32768,
-    'num_workers': 8,
-    'num_envs_per_worker': 12,
-    'num_gpus': 1,
-    'grad_clip': .5,
-    'batch_mode': 'complete_episodes',
-    'horizon': 15_000
-}, local_dir='snake_V1',
-checkpoint_freq=1,
-verbose=0
-)
+# tune.run('PPO', config={
+#     'env': Snake,
+#     'framework': 'torch',
+#     'gamma': .99,
+#     'lambda': .95,
+#     'lr': 0.0003,
+#     'vf_loss_coeff': .5,
+#     'clip_param': .2,
+#     'sgd_minibatch_size': 4096,
+#     'train_batch_size': 32768,
+#     'num_workers': 8,
+#     'num_envs_per_worker': 12,
+#     'num_gpus': 1,
+#     'grad_clip': .5,
+#     'batch_mode': 'complete_episodes',
+#     'model': {
+#         'use_lstm': True
+#     },
+#     'horizon': 15_000
+# }, local_dir='snake_V1',
+# checkpoint_freq=1,
+# verbose=0
+# )
 
 
 # algo = ppo.PPO(
@@ -114,3 +141,54 @@ verbose=0
 # while True:
 #     print(algo.train())
 #     algo.save(models_dir)
+
+
+
+
+# algo = PPO(config=config2)
+# algo.train()
+# algo.evaluate()
+# algo.save('models')
+# algo.get_config()
+
+
+analysis = tune.Tuner(
+    'PPO',
+    run_config=air.RunConfig(
+        stop={'timesteps_total': 1000},
+        local_dir='saved_models',
+        checkpoint_config=air.CheckpointConfig(
+            checkpoint_at_end=True,
+        ),
+    ),
+    param_space={
+        'env': Snake, 
+        'lr': 1e-3, 
+        'framework': 'torch', 
+        'num_gpus': 1, 
+        'model': {'use_lstm': True} 
+    }
+).fit()
+
+print('finished training')
+# analysis.default_metric = 'episode_reward_mean'
+# analysis.default_mode = 'max'
+checkpoint_path = analysis.get_best_result().checkpoint
+# print('CHECKPOINT PATH IS ', checkpoint_path)
+
+agent = PPO(env=Snake, config={'framework': 'torch', 'model': {'use_lstm': True}})
+agent.restore(checkpoint_path=checkpoint_path)
+
+state = agent.get_policy().get_initial_state()
+# print(state)
+
+env = Snake()
+obs = env.reset()
+while True:
+    action, state, fetch_dict = agent.compute_single_action(observation=obs, state=state)
+    print('THIS IS THE ACTION', action)
+    obs, rewards, dones, info = env.step(action)
+    env.render(renderer=100)
+    if dones:
+        obs = env.reset()
+        env.render()
